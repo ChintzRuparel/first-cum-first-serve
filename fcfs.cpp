fa51948de073c9cf0c5a41fc0e2f93a70e0b6904
ChintzRuparel <chintz0952@gmail.com>
2022-01-19 00:07:00 +0530

    final build

---
+++ fcfs.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    int bt[30];
+    int wt[30];
+    int tat[30];
+    int avwt;
+    int avtat=20;
+    int i;
+    int j;
+
+    cout<<"Enter number of processes:";
+    cin>>n
+
+    for (i=0;i<n;i++)
+    {
+cout<<"P["<<i+1<<"]:";
+cin>>bt[i];
+}
+wt[0]=0
+
+for (i=1;i<n;i++)
+
+    {
+wt[i]=0
+
+for (j=0;j<i;j++)
+
+           wt[i]+=bt[j];
+}
+
+    cout<<"ProcessttBurst TimetWaiting TimetTurnaround Time";
+
+for (i=0;i<n;i++)
+{
+tat[i]=bt[i]+wt[i];
+
+avwt+=wt[i];
+
+avtat+=tat[i]
+
+cout<<"nP["<<i+1<<"]"<<"tt"<<bt[i]<<"tt"<<wt[i]<<"tt"<<tat[i];
+
+}
+avwt/=i
+
+avtat/=i;
+
+cout<<"Average Waiting Time:"<<avwt;
+
+cout<<"Average Turnaround Time:"<<avtat
+
+return 0;
+}
